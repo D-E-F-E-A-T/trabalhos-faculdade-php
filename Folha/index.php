e8fc904f41db8f84d08ba52a224059a6731bebde
Mateus Nadal <nlmatt07@hotmail.com>
2020-05-20 11:04:14 -0300

    Update

---
+++ Folha/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="bootstrap.css" media="screen" />
+    <title>Folha de Pagamento</title>
+</head>
+
+<body>
+    <form>
+        <div class="form-group">
+            <label>Nome do funcionário</label>
+            <input type="nome" class="form-control" id="nome" placeholder="Nome">
+        </div>
+        <div class="form-group">
+            <label>Número de horas trabalhadas </label>
+            <input type="nome" class="form-control" id="horas" placeholder="Horas">
+        </div>
+        <div class="form-group">
+            <label>Valor da hora de trabalho</label>
+            <input type="nome" class="form-control" id="valorDasHoras" placeholder="Valor das Horas ">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+
+</html>
